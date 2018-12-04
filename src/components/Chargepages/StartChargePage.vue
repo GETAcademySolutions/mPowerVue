e89ad7f7736272094ed3ce3c219bc0459819f957
Emsaso <34371682+Emsaso@users.noreply.github.com>
2018-12-04 15:21:48 +0100

    style changes

--- src/components/Chargepages/StartChargePage.vue
+++ src/components/Chargepages/StartChargePage.vue
@@ -1,89 +1,101 @@
 <template>
   <div class="StartChargePage component">
-    <keep-alive></keep-alive>
-    <div id="feedbackDiv"></div>
-    <h3 style="font-weight: bold;">My Credits: {{ credits }}</h3>
-    <charging-card v-if="charges[0]" v-bind:charges="charges" />
-    <div class="hugeButton" style="margin-top: 5%; background-color: rgb(0, 150, 50); text-align: left;">
-      <h4>Charge with credits</h4>
-      <p>Charge any device at your convenience. Unlock a charger at any time with your mobile phone.</p>
-      <button id="connectToBluetooth" class="button4" @click="connectToBluetooth" style="border: 2px solid white; width: 100%;">Charge
-        with credits</button>
-    </div>
-    <div class="hugeButton" style="margin-top: 5%; background-color: rgb(255, 255, 255); color: black; border: 1px solid black; text-align: left;">
-      <h4>Charge with code</h4>
-      <p>Pay for charging at one of the mPower stations and get a one time charging code.</p>
-      <button id="connectToBluetooth" class="button4 white" @click="connectToBluetooth" style="border: 2px solid black; color: black; width: 100%;">Charge
-        with code</button>
+      <keep-alive></keep-alive>
+      <div id="feedbackDiv"></div>
+      <h3 style="font-weight: bold;">My Credits: {{ credits }}</h3>
+    <div class="card">
+      <charging-card v-if="charges" v-bind:charges="charges" />
+      <div class="hugeButton card-content" style="background-color: rgb(0, 150, 50); text-align: left;">
+        <h5 style="margin-top: 5%;">Charge with credits</h5>
+        <p style="margin-top: 10%;">Charge any device at your convenience. Unlock a charger at any time with your mobile phone.</p>
+        <button id="connectToBluetooth" class="button4" @click="connectToBluetooth" style="border: 2px solid white; width: 100%; margin-top: 10%;">Charge
+          with credits</button>
+      </div>
+      <div class="hugeButton card-content" style="background-color: rgb(255, 255, 255); color: black; border: 1px solid black; text-align: left;">
+        <h5 style="margin-top: 5%;">Charge with code</h5>
+        <p style="margin-top: 10%;">Pay for charging at one of the mPower stations and get a one time charging code.</p>
+        <button id="connectToBluetooth" class="button4 white" @click="connectToBluetooth" style="border: 2px solid black; color: black; width: 100%; margin-top: 10%;">Charge
+          with code</button>
+      </div>
     </div>
     <button class="button2" @click="home">Back</button>
   </div>
 </template>
 
 <script>
-import db from "@/firebase/init";
-import firebase from "firebase";
-import mPowerBluetoothController from "@/bluetooth/mPowerBluetoothController";
-import mPowerBluetoothControllerDummy from "@/bluetooth/mPowerBluetoothController";
-import ChargingCard from "@/components/History/ChargingCard";
+  import db from "@/firebase/init";
+  import firebase from "firebase";
+  import mPowerBluetoothController from "@/bluetooth/mPowerBluetoothController";
+  import mPowerBluetoothControllerDummy from "@/bluetooth/mPowerBluetoothController";
+  import ChargingCard from "@/components/History/ChargingCard";
 
-export default {
-  name: "StartCharge",
-  data() {
-    return {
-      credits: this.credits,
-      charges: [],      
-      chargeObject: {
+  export default {
+    name: "StartCharge",
+    data() {
+      return {
+        credits: this.credits,
+        charges: [],
+        chargeObject: {
           portNo: 0,
           device: null,
           chargeTime: null,
           batteryLevel: null,
           fullyChargedAt: null
-      }
-    };
-  },
-  props: ["controller"],
-  created() {
-    let user = firebase.auth().currentUser;
-    if (user) {
-      let docRef = db.collection("users").doc(user.uid);
-
-      docRef
-        .get()
-        .then(doc => {
-          if (doc.exists) {
-            this.credits = doc.data().credits;
-          } else {
-            console.log("No such document!");
-          }
-        })
-        .catch(function(error) {
-          console.log("Error getting document:", error);
-        });
-    }
-  },
-  computed: {},
-  methods: {
-    home() {
-      this.$router.push({
-        name: "Home"
-      });
-    },
-    async connectToBluetooth() {
-      console.log("Start charging controller = ", this.controller);
-      if (!this.controller.isConnected) {
-        var controllerName = await this.controller.connect();
-      }
-      this.$router.push({
-        name: "ChargePage",
-        params: {
-          controller: this.controller
         }
-      });
+      };
+    },
+    props: ["controller"],
+    created() {
+      let user = firebase.auth().currentUser;
+      if (user) {
+        let docRef = db.collection("users").doc(user.uid);
+
+        docRef
+          .get()
+          .then(doc => {
+            if (doc.exists) {
+              this.credits = doc.data().credits;
+            } else {
+              console.log("No such document!");
+            }
+          })
+          .catch(function (error) {
+            console.log("Error getting document:", error);
+          });
+      }
+    },
+    computed: {},
+    methods: {
+      home() {
+        this.$router.push({
+          name: "Home"
+        });
+      },
+      async connectToBluetooth() {
+        console.log("Start charging controller = ", this.controller);
+        if (!this.controller.isConnected) {
+          var controllerName = await this.controller.connect();
+        }
+        this.$router.push({
+          name: "ChargePage",
+          params: {
+            controller: this.controller
+          }
+        });
+      }
+    },
+    components: {
+      ChargingCard
     }
-  },
-  components: {
-    ChargingCard
-  }
-};
+  };
+
 </script>
+
+<style scoped>
+p {
+  font-size: 14px;
+}
+.card-content {
+  padding: 20px 50px;
+}
+</style>
--- src/components/History/ChargingCard.vue
+++ src/components/History/ChargingCard.vue
@@ -1,51 +1,75 @@
 <template>
   <div class="component">
-      <div v-for="item in charges" class="card blue-gray" style="text-align: left; border: 1px solid black;">
-        <div class="card-content">
-          <h5>Port: {{ item.portNo }} <a style="float: right; color: green; font-weight: bold;">{{item.batteryLevel}}</a></h5>
-          <h5>Device: <a>{{ item.device }}</a></h5>
-          <h5>Started charging at <a>{{ item.chargeTime }}</a></h5>
-          <h5>Fully charged at <a>{{ item.fullyChargedAt }}</a></h5>
-        </div>
+    <div v-for="item in charges" class="card blue-gray" style="text-align: left; border: 1px solid black;">
+      <div class="card-content">
+        <p>
+          <b>Port:</b> {{ item.portNo }}
+          <a style="float: right; color: green; font-weight: bold;">{{item.batteryLevel}}</a>
+        </p>
+        <p>
+          <b>Device:</b>
+          <a>{{ item.device }}</a>
+        </p>
+        <p>Started charging at
+          <a>{{ item.chargeTime }}</a>
+        </p>
+        <p>Fully charged at
+          <a>{{ item.fullyChargedAt }}</a>
+        </p>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
   var currentdate = new Date();
 
-  var datetime = ('0' + currentdate.getDate()).slice(-2) + "/" +
-    ('0' + (currentdate.getMonth() + 1)).slice(-2) + " - " +
-    ('0' + (currentdate.getHours())).slice(-2) + ":" +
-    ('0' + (currentdate.getMinutes())).slice(-2) + ":" +
-    ('0' + (currentdate.getSeconds())).slice(-2);
-  var datetime2 = ('0' + currentdate.getDate()).slice(-2) + "/" +
-    ('0' + (currentdate.getMonth() + 1)).slice(-2) + " - " +
-    ('0' + (currentdate.getHours() + 1)).slice(-2) + ":" +
-    ('0' + (currentdate.getMinutes())).slice(-2) + ":" +
-    ('0' + (currentdate.getSeconds())).slice(-2);
+  var datetime =
+    ("0" + currentdate.getDate()).slice(-2) +
+    "/" +
+    ("0" + (currentdate.getMonth() + 1)).slice(-2) +
+    " - " +
+    ("0" + currentdate.getHours()).slice(-2) +
+    ":" +
+    ("0" + currentdate.getMinutes()).slice(-2) +
+    ":" +
+    ("0" + currentdate.getSeconds()).slice(-2);
+  var datetime2 =
+    ("0" + currentdate.getDate()).slice(-2) +
+    "/" +
+    ("0" + (currentdate.getMonth() + 1)).slice(-2) +
+    " - " +
+    ("0" + (currentdate.getHours() + 1)).slice(-2) +
+    ":" +
+    ("0" + currentdate.getMinutes()).slice(-2) +
+    ":" +
+    ("0" + currentdate.getSeconds()).slice(-2);
 
   export default {
     data() {
       return {
         charges: [{
-          portNo: "portNo",
-          batteryLevel: "25%",
-          device: "This phone",
-          chargeTime: datetime,
-          fullyChargedAt: datetime2
-        },{
-          portNo: "portNo",
-          batteryLevel: "25%",
-          device: "This phone",
-          chargeTime: datetime,
-          fullyChargedAt: datetime2
-        }]
+            portNo: "portNo",
+            batteryLevel: "25%",
+            device: "This phone",
+            chargeTime: datetime,
+            fullyChargedAt: datetime2
+          },
+          {
+            portNo: "portNo",
+            batteryLevel: "25%",
+            device: "This phone",
+            chargeTime: datetime,
+            fullyChargedAt: datetime2
+          }
+        ]
       };
     },
     methods: {
       history() {
-        this.$router.push({ name: "UserHistory" });
+        this.$router.push({
+          name: "UserHistory"
+        });
       },
       makenew() {
         let charge = null;
@@ -71,3 +95,12 @@
   };
 
 </script>
+
+<style scoped>
+p {
+  font-size: 14px;
+}
+.card-content {
+  padding: 20px 50px;
+}
+</style>
